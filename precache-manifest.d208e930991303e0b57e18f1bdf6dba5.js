f26a96d7818539bac9747d4c9aee5b7a62b89ddd
vladgrecu <51508637+vladgrecu@users.noreply.github.com>
2019-08-25 19:46:40 +0300

    Updates

---
+++ precache-manifest.d208e930991303e0b57e18f1bdf6dba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2ec34afe72bc649deeb902af2d26ce",
+    "url": "/RSPTracker/index.html"
+  },
+  {
+    "revision": "7d86d100597d203b3854",
+    "url": "/RSPTracker/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "097aff7e3d2d4f8e670a",
+    "url": "/RSPTracker/static/css/main.f686f77c.chunk.css"
+  },
+  {
+    "revision": "7d86d100597d203b3854",
+    "url": "/RSPTracker/static/js/2.2852a6e8.chunk.js"
+  },
+  {
+    "revision": "097aff7e3d2d4f8e670a",
+    "url": "/RSPTracker/static/js/main.24bc9905.chunk.js"
+  },
+  {
+    "revision": "e33d1ccc527cb69b16a7",
+    "url": "/RSPTracker/static/js/runtime~main.e3b895ab.js"
+  }
+]);
